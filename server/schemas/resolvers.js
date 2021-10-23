42a402733cd8342b4064eaf29da4df8e0289ca19
Andrew Keiser <webdev410@gmail.com>
2021-10-23 08:30:26 -0700

    cleaned up template

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,80 +1,58 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Thought } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-  Query: {
-    users: async () => {
-      return User.find().populate('thoughts');
-    },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('thoughts');
-    },
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
-    },
-  },
+	Query: {
+		// users: async () => {
+		// 	return User.find().populate("thoughts");
+		// },
+		user: async (parent, { username }) => {
+			return User.findOne({ username });
+		},
+	},
 
-  Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
+	Mutation: {
+		addUser: async (parent, { name, username, email, password }) => {
+			const user = await User.create({ name, username, email, password });
+			const token = signToken(user);
+			return { token, user };
+		},
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
 
-      if (!user) {
-        throw new AuthenticationError('No user found with this email address');
-      }
+			if (!user) {
+				throw new AuthenticationError(
+					"No user found with this email address"
+				);
+			}
 
-      const correctPw = await user.isCorrectPassword(password);
+			const correctPw = await user.isCorrectPassword(password);
 
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
+			if (!correctPw) {
+				throw new AuthenticationError("Incorrect credentials");
+			}
 
-      const token = signToken(user);
+			const token = signToken(user);
 
-      return { token, user };
-    },
-    addThought: async (parent, { thoughtText, thoughtAuthor }) => {
-      const thought = await Thought.create({ thoughtText, thoughtAuthor });
+			return { token, user };
+		},
 
-      await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        { $addToSet: { thoughts: thought._id } }
-      );
-
-      return thought;
-    },
-    addComment: async (parent, { thoughtId, commentText, commentAuthor }) => {
-      return Thought.findOneAndUpdate(
-        { _id: thoughtId },
-        {
-          $addToSet: { comments: { commentText, commentAuthor } },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
-    },
-    removeThought: async (parent, { thoughtId }) => {
-      return Thought.findOneAndDelete({ _id: thoughtId });
-    },
-    removeComment: async (parent, { thoughtId, commentId }) => {
-      return Thought.findOneAndUpdate(
-        { _id: thoughtId },
-        { $pull: { comments: { _id: commentId } } },
-        { new: true }
-      );
-    },
-  },
+		// addComment: async (
+		// 	parent,
+		// 	{ thoughtId, commentText, commentAuthor }
+		// ) => {
+		// 	return Thought.findOneAndUpdate(
+		// 		{ _id: thoughtId },
+		// 		{
+		// 			$addToSet: { comments: { commentText, commentAuthor } },
+		// 		},
+		// 		{
+		// 			new: true,
+		// 			runValidators: true,
+		// 		}
+		// 	);
+		// },
+	},
 };
-
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,53 +1,40 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
-    thoughts: [Thought]!
-  }
+	type User {
+		_id: ID
+		name: String
+		username: String
+		email: String
+		password: String
+	}
 
-  type Thought {
-    _id: ID
-    thoughtText: String
-    thoughtAuthor: String
-    createdAt: String
-    comments: [Comment]!
-  }
+	type Comment {
+		_id: ID
+		commentText: String
+		commentAuthor: String
+		createdAt: String
+	}
 
-  type Comment {
-    _id: ID
-    commentText: String
-    commentAuthor: String
-    createdAt: String
-  }
+	type Auth {
+		token: ID!
+		user: User
+	}
 
-  type Auth {
-    token: ID!
-    user: User
-  }
+	type Query {
+		user(username: String!): User
+		me: User
+	}
 
-  type Query {
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(thoughtId: ID!): Thought
-  }
-
-  type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!, thoughtAuthor: String!): Thought
-    addComment(
-      thoughtId: ID!
-      commentText: String!
-      commentAuthor: String!
-    ): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
-  }
+	type Mutation {
+		addUser(
+			name: String!
+			username: String!
+			email: String!
+			password: String!
+		): Auth
+		login(email: String!, password: String!): Auth
+	}
 `;
 
 module.exports = typeDefs;
