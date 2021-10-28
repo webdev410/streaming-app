5f2f10c675b591026a08be6d329ed1d18afebfc2
tharvey <tharveyster@gmail.com>
2021-10-28 14:53:43 -0400

    Added footer with contributor names

---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; <span className="bigLetter">T</span>odd Harvey, <span className="bigLetter">E</span>d Fatula, <span className="bigLetter">A</span>ndrew Keiser, <span className="bigLetter">M</span>ike Sowers</p>
+    </footer>
+  );
+}
