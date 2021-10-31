33b9ee66443c2fdbafb7e86162b7fcce38875b62
Andrew Keiser <webdev410@gmail.com>
2021-10-31 16:38:16 -0700

    fixed conditional render on homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
 
 	const { loading, data } = useQuery(QUERY_EVENTS);
 	const eventList = data?.events;
-	if (eventList.length === 0) {
+	if (eventList?.length === 0) {
 		return (
 			<div className="ui raised padded container segment">
 				<h4>There are no events.</h4>
